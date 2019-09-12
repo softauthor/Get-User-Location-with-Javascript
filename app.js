cbdc1eac8e86dbadab8c4c9e64fba712840e53d3
SoftAuthor <info@softauthor.com>
2019-09-11 22:08:40 -0400

    Get User Location with Javascript
    I will be first showing you how to get the current user’s location in the form of latitude and longitude coordinates using the HTML5 Geolocation API.
    
    Then, you’re going to learn how to convert the latitude and longitude coordinates into an actual human-readable address using Geocoding API from Google.
    
    Finally, I will show you how to add Autocomplete API which will let users enter their addresses manually when Geolocation API Locator permission was denied or not supported.

---
+++ app.js
@@ -0,0 +1,54 @@
+(function () {
+    var locatorSection = document.getElementById("locator-input-section")
+    var input = document.getElementById("autocomplete");
+
+
+    function init() {
+        var locatorButton = document.getElementById("locator-button");
+        locatorButton.addEventListener("click", locatorButtonPressed)
+
+    }
+
+    function locatorButtonPressed() {
+        locatorSection.classList.add("loading")
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+                getUserAddressBy(position.coords.latitude, position.coords.longitude)
+            },
+            function (error) {
+                locatorSection.classList.remove("loading")
+                alert("The Locator was denied :( Please add your address manually")
+            })
+    }
+
+    function getUserAddressBy(lat, long) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var address = JSON.parse(this.responseText)
+                setAddressToInputField(address.results[0].formatted_address)
+            }
+        };
+        xhttp.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=AIzaSyATZGF2z0UgjOUnn2f5D6VApYJumB10x-4", true);
+        xhttp.send();
+    }
+
+    function setAddressToInputField(address) {
+
+        input.value = address
+        locatorSection.classList.remove("loading")
+    }
+
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(45.4215296, -75.6971931),
+    );
+
+    var options = {
+        bounds: defaultBounds
+    };
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+
+    init()
+
+})();
